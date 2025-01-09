de80ae9113ab13bd80b4b66d72d8da257488bdfb
hemant11ks <ksingh1617@gmail.com>
2025-01-09 23:21:45 +0530

    Adding echo command

--- app/main.py
+++ app/main.py
@@ -2,15 +2,15 @@ import sys
 
 def main():
     while True:
-        
+
         sys.stdout.write("$ ")
         sys.stdout.flush()
 
-        
         command = input().strip()
 
+        # Handle the exit command
         if command.startswith("exit"):
-            # Spliting the exit command
+            # Splitting the command to extract the exit
             parts = command.split()
             if len(parts) == 2 and parts[1].isdigit():
                 exit_code = int(parts[1])
@@ -19,7 +19,14 @@ def main():
                 sys.stdout.write("Usage: exit <code>\n")
                 continue
 
-        
+        # If user type echo command
+        if command.startswith("echo "):
+            # Extract the arguments and print them
+            echo_args = command[5:]  # Everything after "echo "
+            sys.stdout.write(f"{echo_args}\n")
+            continue
+
+        # Now Handling the invalid commands
         sys.stdout.write(f"{command}: command not found\n")
 
 if __name__ == "__main__":
